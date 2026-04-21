0c001e602928c01b7a1619ee265fc53cd932b2ad
katapl <plesovakaterina@gmail.com>
2026-04-21 19:47:24 +0200

    added vitest config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    setupFiles: './src/setupTests.ts',
-    pool: 'forks',
-  },
   base: '/resolution-method-tool/',
 })
